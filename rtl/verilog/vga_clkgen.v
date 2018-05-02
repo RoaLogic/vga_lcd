5867f741e43cccf859bccca5656f9bd4ce29d978
Richard Herveille <richard.herveille@roalogic.com>
2018-05-02 15:03:46 +0200

    Code cleanup

--- rtl/verilog/vga_clkgen.v
+++ rtl/verilog/vga_clkgen.v
@@ -1,39 +1,70 @@
 /////////////////////////////////////////////////////////////////////
-////                                                             ////
-////  WISHBONE rev.B2 compliant VGA/LCD Core; Clock Generator    ////
-////                                                             ////
-////                                                             ////
-////  Author: Richard Herveille                                  ////
-////          richard@asics.ws                                   ////
-////          www.asics.ws                                       ////
-////                                                             ////
-////  Downloaded from: http://www.opencores.org/projects/vga_lcd ////
-////                                                             ////
+//   ,------.                    ,--.                ,--.          //
+//   |  .--. ' ,---.  ,--,--.    |  |    ,---. ,---. `--' ,---.    //
+//   |  '--'.'| .-. |' ,-.  |    |  |   | .-. | .-. |,--.| .--'    //
+//   |  |\  \ ' '-' '\ '-'  |    |  '--.' '-' ' '-' ||  |\ `--.    //
+//   `--' '--' `---'  `--`--'    `-----' `---' `-   /`--' `---'    //
+//             O p e n C o r e s               `---'               //
+//                                                                 //
+//   VGA/LCD Core; Clock Generator                                 //
+//                                                                 //
 /////////////////////////////////////////////////////////////////////
-////                                                             ////
-//// Copyright (C) 2003 Richard Herveille                        ////
-////                    richard@asics.ws                         ////
-////                                                             ////
-//// This source file may be used and distributed without        ////
-//// restriction provided that this copyright statement is not   ////
-//// removed from the file and that any derivative work contains ////
-//// the original copyright notice and the associated disclaimer.////
-////                                                             ////
-////     THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY     ////
-//// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED   ////
-//// TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS   ////
-//// FOR A PARTICULAR PURPOSE. IN NO EVENT SHALL THE AUTHOR      ////
-//// OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,         ////
-//// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES    ////
-//// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE   ////
-//// GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR        ////
-//// BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF  ////
-//// LIABILITY, WHETHER IN  CONTRACT, STRICT LIABILITY, OR TORT  ////
-//// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT  ////
-//// OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE         ////
-//// POSSIBILITY OF SUCH DAMAGE.                                 ////
-////                                                             ////
+//                                                                 //
+//             Copyright (C) 2003 Richard Herveille, OpenCores     //
+//             Copyright (C) 2018 ROA Logic BV                     //
+//             www.roalogic.com                                    //
+//                                                                 //
+//   This source file may be used and distributed without          //
+//   restriction provided that this copyright statement is not     //
+//   removed from the file and that any derivative work contains   //
+//   the original copyright notice and the associated disclaimer.  //
+//                                                                 //
+//      THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY        //
+//   EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED     //
+//   TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS     //
+//   FOR A PARTICULAR PURPOSE. IN NO EVENT SHALL THE AUTHOR OR     //
+//   CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,  //
+//   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  //
+//   NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;  //
+//   LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)      //
+//   HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN     //
+//   CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR  //
+//   OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS          //
+//   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  //
+//                                                                 //
 /////////////////////////////////////////////////////////////////////
+
+// +FHDR -  Semiconductor Reuse Standard File Header Section  -------
+// FILE NAME      : vga_clkgen.v
+// DEPARTMENT     :
+// AUTHOR         : rherveille
+// AUTHOR'S EMAIL :
+// ------------------------------------------------------------------
+// RELEASE HISTORY
+// VERSION DATE        AUTHOR      DESCRIPTION
+// 2.0     2003        rherveille  Last OpenCores Release
+// ------------------------------------------------------------------
+// KEYWORDS : RISC-V PLATFORM LEVEL INTERRUPT CONTROLLER - PLIC
+// ------------------------------------------------------------------
+// PURPOSE  : Clock generation 
+//            source-target matrix.
+// ------------------------------------------------------------------
+// PARAMETERS
+//  PARAM NAME        RANGE  DESCRIPTION              DEFAULT UNITS
+//  HAS_DVI           [0,1]  Implement DVI clocks?    1
+// ------------------------------------------------------------------
+// REUSE ISSUES 
+//   Reset Strategy      : syncronous, active high
+//   Clock Domains       : pclk_i
+//   Critical Timing     : 
+//   Test Features       : 
+//   Asynchronous I/F    : Fully asynchronous block
+//   Scan Methodology    : na
+//   Instantiations      : none
+//   Synthesizable (y/n) : Yes
+//   Other               : End-points (registers) are in the
+//                         plic_target module
+// -FHDR-------------------------------------------------------------
 
 /////////////////////////////////////////////////////////////////////
 //                                                                 //
@@ -43,93 +74,71 @@
 // are generated synchronous to the input clock, special care must //
 // be taken during synthesis and physical layout.                  //
 //                                                                 //
+// Alternatively replace this block with a technology specific     //
+// implementation.                                                 //
+//                                                                 //
 /////////////////////////////////////////////////////////////////////
 
 
+module vga_clkgen #(
+  parameter HAS_DVI = 1
+)
+(
+  // inputs & outputs
+  input      pclk_i,       // pixel clock in
+  input      rst_i,        // synchronous active high reset input
 
-//  CVS Log
-//
-//  $Id: vga_clkgen.v,v 1.1 2003-05-07 14:43:01 rherveille Exp $
-//
-//  $Date: 2003-05-07 14:43:01 $
-//  $Revision: 1.1 $
-//  $Author: rherveille $
-//  $Locker:  $
-//  $State: Exp $
-//
-// Change History:
-//               $Log: not supported by cvs2svn $
-//
+  output reg pclk_o,       // pixel clock out
 
-//synopsys translate_off
-`include "timescale.v"
-//synopsys translate_on
+  output reg dvi_pclk_p_o  // dvi cpclk+ output
+  output reg dvi_pclk_n_o  // dvi cpclk- output
 
-`include "vga_defines.v"
-
-module vga_clkgen (
-	pclk_i, rst_i, pclk_o, dvi_pclk_p_o, dvi_pclk_m_o, pclk_ena_o
+  output reg pclk_ena_o    // pixel clock enable output
 );
 
-	// inputs & outputs
 
-	input  pclk_i;       // pixel clock in
-	input  rst_i;        // reset input
+//////////////////////////////////
+//
+// module body
+//
 
-	output pclk_o;       // pixel clock out
+  // These should be registers in or near IO buffers
+  always @(posedge pclk_i)
+    if (rst_i)
+    begin
+        dvi_pclk_p_o <= 1'b0;
+        dvi_pclk_m_o <= 1'b0;
+    end
+    else
+    begin
+        dvi_pclk_p_o <= ~dvi_pclk_p_o;
+        dvi_pclk_m_o <=  dvi_pclk_p_o;
+    end
 
-	output dvi_pclk_p_o; // dvi cpclk+ output
-	output dvi_pclk_m_o; // dvi cpclk- output
+generate
+  if (HAS_DVI == 1)
+  begin
+      // DVI circuit
+      // pixel clock output is half of the pixel clock input
 
-	output pclk_ena_o;   // pixel clock enable output
-
-	//
-	// variable declarations
-	//
-	reg dvi_pclk_p_o;
-	reg dvi_pclk_m_o;
-
-	//////////////////////////////////
-	//
-	// module body
-	//
-
-	// These should be registers in or near IO buffers
-	always @(posedge pclk_i)
-	  if (rst_i) begin
-	    dvi_pclk_p_o <= #1 1'b0;
-	    dvi_pclk_m_o <= #1 1'b0;
-	  end else begin
-	    dvi_pclk_p_o <= #1 ~dvi_pclk_p_o;
-	    dvi_pclk_m_o <= #1 dvi_pclk_p_o;
-	  end
+      always @(posedge pclk_i)
+        if (rst_i) pclk_o <=  1'b0;
+        else       pclk_o <= ~pclk_o;
 
 
-`ifdef VGA_12BIT_DVI
-	// DVI circuit
-	// pixel clock is half of the input pixel clock
+      always @(posedge pclk_i)
+        if (rst_i) pclk_ena_o <=  1'b1;
+        else       pclk_ena_o <= ~pclk_ena_o;
+  end
+  else
+  begin
+      // No DVI circuit
+      // Simply reroute the pixel input clock input
 
-	reg pclk_o, pclk_ena_o;
-
-	always @(posedge pclk_i)
-	  if (rst_i)
-	    pclk_o <= #1 1'b0;
-	  else
-	    pclk_o <= #1 ~pclk_o;
-
-	always @(posedge pclk_i)
-	  if (rst_i)
-	    pclk_ena_o <= #1 1'b1;
-	  else
-	    pclk_ena_o <= #1 ~pclk_ena_o;
-
-`else
-	// No DVI circuit
-	// Simply reroute the pixel clock input
-
-	assign pclk_o     = pclk_i;
-	assign pclk_ena_o = 1'b1;
-`endif
+      always @* pclk_o     = pclk_i;
+      always @* pclk_ena_o = 1'b1;
+  end
+endgenerate
 
 endmodule
 
