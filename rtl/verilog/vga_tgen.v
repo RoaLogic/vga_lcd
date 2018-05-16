82abc8e11d2cc8ccdc402c87ba812c5d6ab4e04c
Richard Herveille <richard.herveille@roalogic.com>
2018-05-16 18:26:57 +0200

    New timing generator

--- rtl/verilog/vga_tgen.v
+++ rtl/verilog/vga_tgen.v
@@ -1,135 +1,143 @@
 /////////////////////////////////////////////////////////////////////
-////                                                             ////
-////  WISHBONE rev.B2 compliant VGA/LCD Core; Timing Generator   ////
-////  Horizontal and Vertical Timing Generator                   ////
-////                                                             ////
-////  Author: Richard Herveille                                  ////
-////          richard@asics.ws                                   ////
-////          www.asics.ws                                       ////
-////                                                             ////
-////  Downloaded from: http://www.opencores.org/projects/vga_lcd ////
-////                                                             ////
+//   ,------.                    ,--.                ,--.          //
+//   |  .--. ' ,---.  ,--,--.    |  |    ,---. ,---. `--' ,---.    //
+//   |  '--'.'| .-. |' ,-.  |    |  |   | .-. | .-. |,--.| .--'    //
+//   |  |\  \ ' '-' '\ '-'  |    |  '--.' '-' ' '-' ||  |\ `--.    //
+//   `--' '--' `---'  `--`--'    `-----' `---' `-   /`--' `---'    //
+//             O p e n C o r e s               `---'               //
+//                                                                 //
+//   VGA/LCD Core; Timing Generator                                //
+//                                                                 //
 /////////////////////////////////////////////////////////////////////
-////                                                             ////
-//// Copyright (C) 2001 Richard Herveille                        ////
-////                    richard@asics.ws                         ////
-////                                                             ////
-//// This source file may be used and distributed without        ////
-//// restriction provided that this copyright statement is not   ////
-//// removed from the file and that any derivative work contains ////
-//// the original copyright notice and the associated disclaimer.////
-////                                                             ////
-////     THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY     ////
-//// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED   ////
-//// TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS   ////
-//// FOR A PARTICULAR PURPOSE. IN NO EVENT SHALL THE AUTHOR      ////
-//// OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,         ////
-//// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES    ////
-//// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE   ////
-//// GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR        ////
-//// BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF  ////
-//// LIABILITY, WHETHER IN  CONTRACT, STRICT LIABILITY, OR TORT  ////
-//// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT  ////
-//// OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE         ////
-//// POSSIBILITY OF SUCH DAMAGE.                                 ////
-////                                                             ////
+//                                                                 //
+//             Copyright (C) 2001 Richard Herveille, OpenCores     //
+//             Copyright (C) 2018 Roa Logic BV                     //
+//             www.roalogic.com                                    //
+//                                                                 //
+//   This source file may be used and distributed without          //
+//   restriction provided that this copyright statement is not     //
+//   removed from the file and that any derivative work contains   //
+//   the original copyright notice and the associated disclaimer.  //
+//                                                                 //
+//      THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY        //
+//   EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED     //
+//   TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS     //
+//   FOR A PARTICULAR PURPOSE. IN NO EVENT SHALL THE AUTHOR OR     //
+//   CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,  //
+//   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  //
+//   NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;  //
+//   LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)      //
+//   HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN     //
+//   CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR  //
+//   OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS          //
+//   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  //
+//                                                                 //
 /////////////////////////////////////////////////////////////////////
 
-//  CVS Log
-//
-//  $Id: vga_tgen.v,v 1.5 2003-05-07 09:48:54 rherveille Exp $
-//
-//  $Date: 2003-05-07 09:48:54 $
-//  $Revision: 1.5 $
-//  $Author: rherveille $
-//  $Locker:  $
-//  $State: Exp $
-//
-// Change History:
-//               $Log: not supported by cvs2svn $
-//               Revision 1.4  2002/01/28 03:47:16  rherveille
-//               Changed counter-library.
-//               Changed vga-core.
-//               Added 32bpp mode.
-//
+
+// +FHDR -  Semiconductor Reuse Standard File Header Section  -------
+// FILE NAME      : vga_tgen.v
+// DEPARTMENT     :
+// AUTHOR         : rherveille
+// AUTHOR'S EMAIL :
+// ------------------------------------------------------------------
+// RELEASE HISTORY
+// VERSION DATE        AUTHOR      DESCRIPTION
+// 2.0     2001        rherveille  Last OpenCores Release
+// ------------------------------------------------------------------
+// KEYWORDS : VGA_LCD OPENCORES                                 
+// ------------------------------------------------------------------
+// PURPOSE  : Horizontal and Vertical Timing Generator
+// ------------------------------------------------------------------
+// PARAMETERS
+//  PARAM NAME        RANGE  DESCRIPTION              DEFAULT UNITS
+// ------------------------------------------------------------------
+// REUSE ISSUES 
+//   Reset Strategy      : synchronous active high rst_i
+//   Clock Domains       : clk_i
+//   Critical Timing     : 
+//   Test Features       : 
+//   Asynchronous I/F    : no 
+//   Scan Methodology    : na
+//   Instantiations      : vga_vtim
+//   Synthesizable (y/n) : Yes
+//   Other               :
+// -FHDR-------------------------------------------------------------
+
 
 module vga_tgen(
-	clk, clk_ena, rst,
-	Thsync, Thgdel, Thgate, Thlen, Tvsync, Tvgdel, Tvgate, Tvlen,
-	eol, eof, gate, hsync, vsync, csync, blank
-	);
+  input         clk_i,
+  input         clk_ena_i,
+  input         rst_i,
 
-	// inputs & outputs
-	input clk;
-	input clk_ena;
-	input rst;
+  // horizontal timing settings inputs
+  input  [ 7:0] thsync_i, // horizontal sync pule width (in pixels)
+  input  [ 7:0] thgdel_i, // horizontal gate delay
+  input  [15:0] thgate_i, // horizontal gate (number of visible pixels per line)
+  input  [15:0] thlen_i,  // horizontal length (number of pixels per line)
 
-	// horizontal timing settings inputs
-	input [ 7:0] Thsync; // horizontal sync pule width (in pixels)
-	input [ 7:0] Thgdel; // horizontal gate delay
-	input [15:0] Thgate; // horizontal gate (number of visible pixels per line)
-	input [15:0] Thlen;  // horizontal length (number of pixels per line)
+  // vertical timing settings inputs
+  input  [ 7:0] tvsync_i, // vertical sync pule width (in pixels)
+  input  [ 7:0] tvgdel_i, // vertical gate delay
+  input  [15:0] tvgate_i, // vertical gate (number of visible pixels per line)
+  input  [15:0] tvlen_i,  // vertical length (number of pixels per line)
 
-	// vertical timing settings inputs
-	input [ 7:0] Tvsync; // vertical sync pule width (in pixels)
-	input [ 7:0] Tvgdel; // vertical gate delay
-	input [15:0] Tvgate; // vertical gate (number of visible pixels per line)
-	input [15:0] Tvlen;  // vertical length (number of pixels per line)
+  // outputs
+  output        eol_o,    // end of line
+  output        eof_o,    // end of frame
+  output        gate_o,   // vertical AND horizontal gate (logical AND function)
 
-	// outputs
-	output eol;  // end of line
-	output eof;  // end of frame
-	output gate; // vertical AND horizontal gate (logical AND function)
+  output        hsync_o,  // horizontal sync pulse
+  output        vsync_o,  // vertical sync pulse
+  output        csync_o,  // composite sync
+  output        blank_o   // blank signal
+);
 
-	output hsync; // horizontal sync pulse
-	output vsync; // vertical sync pulse
-	output csync; // composite sync
-	output blank; // blank signal
-
-	//
-	// variable declarations
-	//
-	wire Hgate, Vgate;
-	wire Hdone;
-
-	//
-	// module body
-	//
-
-	// hookup horizontal timing generator
-	vga_vtim hor_gen(
-		.clk(clk),
-		.ena(clk_ena),
-		.rst(rst),
-		.Tsync(Thsync),
-		.Tgdel(Thgdel),
-		.Tgate(Thgate),
-		.Tlen(Thlen),
-		.Sync(hsync),
-		.Gate(Hgate),
-		.Done(Hdone)
-	);
+  //---------------------------------------------
+  // variable declarations
+  //
+  wire hgate, vgate;
 
 
-	// hookup vertical timing generator
-	wire vclk_ena = Hdone & clk_ena;
+  //---------------------------------------------
+  // module body
+  //
 
-	vga_vtim ver_gen(
-		.clk(clk),
-		.ena(vclk_ena),
-		.rst(rst),
-		.Tsync(Tvsync),
-		.Tgdel(Tvgdel),
-		.Tgate(Tvgate),
-		.Tlen(Tvlen),
-		.Sync(vsync),
-		.Gate(Vgate),
-		.Done(eof)
-	);
+  // hookup horizontal timing generator
+  vga_vtim htim_inst (
+    .clk_i   ( clk_i     ),
+    .ena_i   ( clk_ena_i ),
+    .rst_i   ( rst_i     ),
+    .tsync_i ( thsync_i  ),
+    .tgdel_i ( thgdel_i  ),
+    .tgate_i ( thgate_i  ),
+    .tlen_i  ( thlen_i   ),
+    .sync_o  ( hsync_o   ),
+    .gate_o  ( hgate     ),
+    .done_o  ( eol_o     )
+  );
 
-	// assign outputs
-	assign eol  = Hdone;
-	assign gate = Hgate & Vgate;
-	assign csync = hsync | vsync;
-	assign blank = ~gate;
+
+  // hookup vertical timing generator
+  wire vclk_ena = eol_o & clk_ena_i;
+
+  vga_vtim vtim_inst (
+    .clk_i   ( clk_i    ),
+    .ena_i   ( vclk_ena ),
+    .rst_i   ( rst_i    ),
+    .tsync_i ( tvsync_i ),
+    .tgdel_i ( tvgdel_i ),
+    .tgate_i ( tvgate_i ),
+    .tlen_i  ( tvlen_i  ),
+    .sync_o  ( vsync_o  ),
+    .gate_o  ( vgate    ),
+    .done_o  ( eof_o    )
+  );
+
+
+  // assign outputs
+  assign gate_o  = hgate & vgate;
+  assign csync_o = hsync_o | vsync_o;
+  assign blank_o = ~gate_o;
+
 endmodule
--- rtl/verilog/vga_vtim.v
+++ rtl/verilog/vga_vtim.v
@@ -1,171 +1,158 @@
 /////////////////////////////////////////////////////////////////////
-////                                                             ////
-////  WISHBONE rev.B2 compliant VGA/LCD Core; Timing Generator   ////
-////  Video Timing Generator                                     ////
-////                                                             ////
-////  Author: Richard Herveille                                  ////
-////          richard@asics.ws                                   ////
-////          www.asics.ws                                       ////
-////                                                             ////
-////  Downloaded from: http://www.opencores.org/projects/vga_lcd ////
-////                                                             ////
+//   ,------.                    ,--.                ,--.          //
+//   |  .--. ' ,---.  ,--,--.    |  |    ,---. ,---. `--' ,---.    //
+//   |  '--'.'| .-. |' ,-.  |    |  |   | .-. | .-. |,--.| .--'    //
+//   |  |\  \ ' '-' '\ '-'  |    |  '--.' '-' ' '-' ||  |\ `--.    //
+//   `--' '--' `---'  `--`--'    `-----' `---' `-   /`--' `---'    //
+//             O p e n C o r e s               `---'               //
+//                                                                 //
+//   VGA/LCD Core; Timing Generator                                //
+//                                                                 //
 /////////////////////////////////////////////////////////////////////
-////                                                             ////
-//// Copyright (C) 2001 Richard Herveille                        ////
-////                    richard@asics.ws                         ////
-////                                                             ////
-//// This source file may be used and distributed without        ////
-//// restriction provided that this copyright statement is not   ////
-//// removed from the file and that any derivative work contains ////
-//// the original copyright notice and the associated disclaimer.////
-////                                                             ////
-////     THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY     ////
-//// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED   ////
-//// TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS   ////
-//// FOR A PARTICULAR PURPOSE. IN NO EVENT SHALL THE AUTHOR      ////
-//// OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,         ////
-//// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES    ////
-//// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE   ////
-//// GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR        ////
-//// BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF  ////
-//// LIABILITY, WHETHER IN  CONTRACT, STRICT LIABILITY, OR TORT  ////
-//// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT  ////
-//// OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE         ////
-//// POSSIBILITY OF SUCH DAMAGE.                                 ////
-////                                                             ////
+//                                                                 //
+//             Copyright (C) 2001 Richard Herveille, OpenCores     //
+//             Copyright (C) 2018 Roa Logic BV                     //
+//             www.roalogic.com                                    //
+//                                                                 //
+//   This source file may be used and distributed without          //
+//   restriction provided that this copyright statement is not     //
+//   removed from the file and that any derivative work contains   //
+//   the original copyright notice and the associated disclaimer.  //
+//                                                                 //
+//      THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY        //
+//   EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED     //
+//   TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS     //
+//   FOR A PARTICULAR PURPOSE. IN NO EVENT SHALL THE AUTHOR OR     //
+//   CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,  //
+//   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  //
+//   NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;  //
+//   LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)      //
+//   HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN     //
+//   CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR  //
+//   OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS          //
+//   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  //
+//                                                                 //
 /////////////////////////////////////////////////////////////////////
 
-//  CVS Log
-//
-//  $Id: vga_vtim.v,v 1.8 2003-05-07 09:48:54 rherveille Exp $
-//
-//  $Date: 2003-05-07 09:48:54 $
-//  $Revision: 1.8 $
-//  $Author: rherveille $
-//  $Locker:  $
-//  $State: Exp $
-//
-// Change History:
-//               $Log: not supported by cvs2svn $
-//               Revision 1.7  2003/03/19 12:50:45  rherveille
-//               Changed timing generator; made it smaller and easier.
-//
-//               Revision 1.6  2002/04/20 10:02:39  rherveille
-//               Changed video timing generator.
-//               Changed wishbone master vertical gate count code.
-//               Fixed a potential bug in the wishbone slave (cursor color register readout).
-//
-//               Revision 1.5  2002/01/28 03:47:16  rherveille
-//               Changed counter-library.
-//               Changed vga-core.
-//               Added 32bpp mode.
-//
+
+// +FHDR -  Semiconductor Reuse Standard File Header Section  -------
+// FILE NAME      : vga_vtim.v
+// DEPARTMENT     :
+// AUTHOR         : rherveille
+// AUTHOR'S EMAIL :
+// ------------------------------------------------------------------
+// RELEASE HISTORY
+// VERSION DATE        AUTHOR      DESCRIPTION
+// 2.0     2001        rherveille  Last OpenCores Release
+// ------------------------------------------------------------------
+// KEYWORDS : VGA_LCD OPENCORES                                 
+// ------------------------------------------------------------------
+// PURPOSE  : Timing Generator
+// ------------------------------------------------------------------
+// PARAMETERS
+//  PARAM NAME        RANGE  DESCRIPTION              DEFAULT UNITS
+// ------------------------------------------------------------------
+// REUSE ISSUES 
+//   Reset Strategy      : synchronous active high rst_i
+//   Clock Domains       : clk_i
+//   Critical Timing     : 
+//   Test Features       : 
+//   Asynchronous I/F    : no 
+//   Scan Methodology    : na
+//   Instantiations      : none
+//   Synthesizable (y/n) : Yes
+//   Other               :
+// -FHDR-------------------------------------------------------------
 
 
-module vga_vtim(clk, ena, rst, Tsync, Tgdel, Tgate, Tlen, Sync, Gate, Done);
-	// inputs & outputs
-	input clk; // master clock
-	input ena; // count enable
-	input rst; // synchronous active high reset
+module vga_vtim (
+  input             clk_i,   // master clock
+  input             ena_i,   // count enable
+  input             rst_i,   // synchronous active high reset
 
-	input [ 7:0] Tsync; // sync duration
-	input [ 7:0] Tgdel; // gate delay
-	input [15:0] Tgate; // gate length
-	input [15:0] Tlen;  // line time / frame time
+  input      [ 7:0] tsync_i, // sync duration
+  input      [ 7:0] tgdel_i, // gate delay
+  input      [15:0] tgate_i, // gate length
+  input      [15:0] tlen_i,  // line time / frame time
 
-	output Sync; // synchronization pulse
-	output Gate; // gate
-	output Done; // done with line/frame
-	reg Sync;
-	reg Gate;
-	reg Done;
+  output reg        sync_o,  // synchronization pulse
+  output reg        gate_o,  // gate
+  output reg        done_o   // done with line/frame
+);
 
-	//
-	// module body
-	//
+  //---------------------------------------------
+  // Constants
+  //
+  localparam [1:0] SYNC = 2'b00,
+                   GDEL = 2'b01,
+                   GATE = 2'b11,
+                   LEN  = 2'b10;
 
-	// generate timing statemachine
-	reg  [15:0] cnt, cnt_len;
-	wire [16:0] cnt_nxt, cnt_len_nxt;
-	wire        cnt_done, cnt_len_done;
+  //---------------------------------------------
+  // Variables
+  //
+  reg  [ 1:0] fsm_state;
 
-	assign cnt_nxt = {1'b0, cnt} -17'h1;
-	assign cnt_done = cnt_nxt[16];
+  reg  [15:0] cnt,      cnt_len;
+  wire [15:0] cnt_nxt,  cnt_len_next;
+  wire        cnt_done, cnt_len_done;
 
-	assign cnt_len_nxt = {1'b0, cnt_len} -17'h1;
-	assign cnt_len_done = cnt_len_nxt[16];
 
-	reg [4:0] state;
-	parameter [4:0] idle_state = 5'b00001;
-	parameter [4:0] sync_state = 5'b00010;
-	parameter [4:0] gdel_state = 5'b00100;
-	parameter [4:0] gate_state = 5'b01000;
-	parameter [4:0] len_state  = 5'b10000;
+  //---------------------------------------------
+  // module body
+  //
 
-	always @(posedge clk)
-	  if (rst)
-	    begin
-	        state   <= #1 idle_state;
-	        cnt     <= #1 16'h0;
-	        cnt_len <= #1 16'b0;
-	        Sync    <= #1 1'b0;
-	        Gate    <= #1 1'b0;
-	        Done    <= #1 1'b0;
-	    end
-	  else if (ena)
-	    begin
-	        cnt     <= #1 cnt_nxt[15:0];
-	        cnt_len <= #1 cnt_len_nxt[15:0];
+  assign {cnt_done,    cnt_nxt    } = {1'b0, cnt    } -1;
+  assign {cnt_len_done,cnt_len_nxt} = {1'b0, cnt_len} -1;
 
-	        Done    <= #1 1'b0;
 
-	        case (state) // synopsys full_case parallel_case
-	          idle_state:
-	            begin
-	                state   <= #1 sync_state;
-	                cnt     <= #1 Tsync;
-	                cnt_len <= #1 Tlen;
+  // State Machine
+  always @(posedge clk_i)
+    if (rst_i)
+    begin
+        fsm_state <= SYNC;
+        cnt       <= tsync_i;
+        cnt_len   <= tlen_i;
+        sync_o    <= 1'b0;
+        gate_o    <= 1'b0;
+        done_o    <= 1'b0;
+    end
+    else if (ena_i)
+    begin
+       cnt     <= cnt_nxt;
+       cnt_len <= cnt_len_nxt;
+       done_o  <= 1'b0;
 
-	                Sync    <= #1 1'b1;
-	            end
+       case (fsm_state)
+         SYNC: if (cnt_done)
+               begin
+                   fsm_state <= GDEL;
+                   cnt       <= tgdel_i;
+                   sync_o    <= 1'b0;
+               end
 
-	          sync_state:
-	            if (cnt_done)
-	              begin
-	                  state <= #1 gdel_state;
-	                  cnt   <= #1 Tgdel;
+         GDEL: if (cnt_done)
+               begin
+                   fsm_state <= GATE;
+                   cnt       <= tgate_i;
+                   gate_o    <= 1'b1;
+               end
 
-	                  Sync  <= #1 1'b0;
-	              end
+         GATE: if (cnt_done)
+               begin
+                   fsm_state <= LEN;
+                   gate_o    <= 1'b0;
+               end
 
-	          gdel_state:
-	            if (cnt_done)
-	              begin
-	                  state <= #1 gate_state;
-	                  cnt   <= #1 Tgate;
+         LEN : if (cnt_len_done)
+               begin
+                   fsm_state <= SYNC;
+                   cnt       <= tsync_i;
+                   cnt_len   <= tlen_i;
+                   sync_o    <= 1'b1;
+                   done_o    <= 1'b1;
+               end
+       endcase
+   end
 
-	                  Gate  <= #1 1'b1;
-	              end
-
-	          gate_state:
-	            if (cnt_done)
-	              begin
-	                  state <= #1 len_state;
-
-	                  Gate  <= #1 1'b0;
-	              end
-
-	          len_state:
-	            if (cnt_len_done)
-	              begin
-	                  state   <= #1 sync_state;
-	                  cnt     <= #1 Tsync;
-	                  cnt_len <= #1 Tlen;
-
-	                  Sync    <= #1 1'b1;
-	                  Done    <= #1 1'b1;
-	              end
-
-	        endcase
-	    end
 endmodule
